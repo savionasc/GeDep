563b7a63f58853a78ee7f32a08af95a68edec854
savionasc <savionasc@gmail.com>
2020-10-16 22:52:49 -0300

    Classes de Vendas
    Adicionando classes de Vendas de produtos no sistema

---
+++ src/controller/ControllerVendasProdutos.java
@@ -0,0 +1,59 @@
+package controller;
+
+import model.ModelVendasProdutos;
+import DAO.DAOVendasProdutos;
+import java.util.ArrayList;
+
+/**
+*
+* @author Sávio Nascimento
+*/
+public class ControllerVendasProdutos {
+
+    private DAOVendasProdutos daoVendasProdutos = new DAOVendasProdutos();
+
+    /**
+    * grava VendasProdutos
+    * @param pModelVendasProdutos
+    * @return int
+    */
+    public int salvarVendasProdutosController(ModelVendasProdutos pModelVendasProdutos){
+        return this.daoVendasProdutos.salvarVendasProdutosDAO(pModelVendasProdutos);
+    }
+
+    /**
+    * recupera VendasProdutos
+    * @param pIdVendaProduto
+    * @return ModelVendasProdutos
+    */
+    public ModelVendasProdutos getVendasProdutosController(int pIdVendaProduto){
+        return this.daoVendasProdutos.getVendasProdutosDAO(pIdVendaProduto);
+    }
+
+    /**
+    * recupera uma lista deVendasProdutos
+    * @param pIdVendaProduto
+    * @return ArrayList
+    */
+    public ArrayList<ModelVendasProdutos> getListaVendasProdutosController(){
+        return this.daoVendasProdutos.getListaVendasProdutosDAO();
+    }
+
+    /**
+    * atualiza VendasProdutos
+    * @param pModelVendasProdutos
+    * @return boolean
+    */
+    public boolean atualizarVendasProdutosController(ModelVendasProdutos pModelVendasProdutos){
+        return this.daoVendasProdutos.atualizarVendasProdutosDAO(pModelVendasProdutos);
+    }
+
+    /**
+    * exclui VendasProdutos
+    * @param pIdVendaProduto
+    * @return boolean
+    */
+    public boolean excluirVendasProdutosController(int pIdVendaProduto){
+        return this.daoVendasProdutos.excluirVendasProdutosDAO(pIdVendaProduto);
+    }
+}
